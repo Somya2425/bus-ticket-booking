2c824b1c97b7ae995372aeccf17fe02fe5a97540
Somya2425 <somya36056@gmail.com>
2026-03-28 22:39:38 +0530

    Made an api to get no. of available seats by tripId.

--- src/main/java/com/cg/busbooking/controller/TripController.java
+++ src/main/java/com/cg/busbooking/controller/TripController.java
@@ -1,4 +1,24 @@
 package com.cg.busbooking.controller;
 
+import com.cg.busbooking.dto.response.AvailableSeatsResponseDto;
+import com.cg.busbooking.service.TripService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.EntityResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/trip")
 public class TripController {
+  private final TripService tripService;
+
+  @GetMapping("/seats")
+  public ResponseEntity<AvailableSeatsResponseDto> getAvailableSeats(@RequestParam Integer tripId){
+      return ResponseEntity.ok(tripService.getAvailableSeatsForTrip(tripId));
+  }
+
 }
