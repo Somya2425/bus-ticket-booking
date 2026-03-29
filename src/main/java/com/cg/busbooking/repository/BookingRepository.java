4fe8cf3149238014eaea9fe4417e4befc03f2feb
shalinifabulous <chinkiwork28@gmail.com>
2026-03-29 16:48:32 +0530

    Added booking repository queries

--- src/main/java/com/cg/busbooking/repository/BookingRepository.java
+++ src/main/java/com/cg/busbooking/repository/BookingRepository.java
@@ -2,8 +2,15 @@ package com.cg.busbooking.repository;
 
 import com.cg.busbooking.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking,Integer> {
+    @Query("SELECT b FROM Booking b " +
+            "JOIN Payment p ON b.bookingId = p.booking.bookingId " +
+            "WHERE p.customer.customerId = :customerId")
+    List<Booking> findBookingsByCustomerId(Integer customerId);
 }
