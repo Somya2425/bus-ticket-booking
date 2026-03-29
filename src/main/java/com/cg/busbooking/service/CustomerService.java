2e74865879266d25628ede767bfad78307f57d43
shalinifabulous <chinkiwork28@gmail.com>
2026-03-29 16:48:19 +0530

    Added customer service interface

--- src/main/java/com/cg/busbooking/service/CustomerService.java
+++ src/main/java/com/cg/busbooking/service/CustomerService.java
@@ -1,4 +1,14 @@
 package com.cg.busbooking.service;
 
+import com.cg.busbooking.entity.Booking;
+import com.cg.busbooking.entity.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
+    List<Customer> getCustomerByNameAndAddress(String name, String address);
+
+    List<Booking> getCustomerBookings(Integer customerId);
+
+    List<Customer> getCustomersByAgency(Integer agencyId);
 }
