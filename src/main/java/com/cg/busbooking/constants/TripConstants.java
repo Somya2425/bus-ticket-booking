06380909135c9cc102ee508c9306133509426366
shalinifabulous <chinkiwork28@gmail.com>
2026-03-29 17:22:38 +0530

    Added constants, dto, exception from development branch

---
+++ src/main/java/com/cg/busbooking/constants/TripConstants.java
@@ -0,0 +1,11 @@
+package com.cg.busbooking.constants;
+
+public class TripConstants {
+    private TripConstants(){
+
+    }
+
+    public static final String TRIP_NOT_FOUND = "Trip not found for id: %d";
+    public static final String AGENCY_NOT_FOUND = "Agency not found for id: %d";
+
+}
---
+++ src/main/java/com/cg/busbooking/dto/response/AvailableSeatsResponseDto.java
@@ -0,0 +1,18 @@
+package com.cg.busbooking.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AvailableSeatsResponseDto {
+    Integer tripId;
+    Integer availableSeats;
+    long bookedSeatsCount;
+
+
+}
